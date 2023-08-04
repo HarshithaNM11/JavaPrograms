1f65b89713664095f785013a374e5330005d3b12
unknown <HarshithaNM11>
2023-08-04 20:07:22 +0530

    chaining

---
+++ 04-08-2023-chaining/04-08-2023-Diamond/src/com/xworkz/boot/DiamondRunner.java
@@ -0,0 +1,25 @@
+package com.xworkz.boot;
+
+import com.xworkz.app.FancyDaimond;
+
+public class DiamondRunner {
+	public static void main(String[] args) {
+		System.out.println("invoking main in DaimonRunner");
+
+		FancyDaimond fancyDaimond = new FancyDaimond();
+		System.out.println(fancyDaimond.carat);
+		System.out.println(fancyDaimond.clarity);
+		System.out.println(fancyDaimond.color);
+		System.out.println(fancyDaimond.shape);
+
+		System.out.println("*******************");
+
+		FancyDaimond fancyDaimond1 = new FancyDaimond("Princess",0.75,"Red","VS2");
+		System.out.println(fancyDaimond1.carat);
+		System.out.println(fancyDaimond1.clarity);
+		System.out.println(fancyDaimond1.color);
+		System.out.println(fancyDaimond1.shape);
+
+	}
+
+}
